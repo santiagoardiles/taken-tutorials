5235f72c240c8ac72763068174a1fb0ec4811207
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2021-02-11 18:44:30 +0100

    Not really the first commit.

---
+++ simple-laravel-crud/README.md
@@ -0,0 +1,1 @@
+# Søren Spangsberg Jørgensen
---
+++ simple-laravel-crud/app/Http/Controllers/ProductController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+
+        return view("products.index", compact("products"))->with(request()->input("page"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("products.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the user's input.
+        $request->validate([
+            "name" => "required",
+            "detail" => "required",
+        ]);
+
+        // Create a new product.
+        Product::create($request->all());
+
+        // Redirect the user.
+        return redirect()->route("products.index")->with("success", "Product created successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return view("products.show", compact("product"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return view("products.edit", compact("product"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        // Validate the user's input.
+        $request->validate([
+            "name" => "required",
+            "detail" => "required",
+        ]);
+
+        // Create a new product.
+        $product->update($request->all());
+
+        // Redirect the user.
+        return redirect()->route("products.index")->with("success", "Product updated successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        // Delete the product.
+        $product->delete();
+
+        // Redirect and show success message.
+        return redirect()->route("products.index")->with("success", "Product deleted successfully");
+    }
+}
